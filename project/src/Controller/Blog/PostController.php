8d28c78e5f71791bca06c72c11acc0ef0631aa84
leeandri <rl.tsirih@gmail.com>
2023-06-20 23:15:04 +0300

    📝Display item details using ParamConverter

--- project/src/Controller/Blog/PostController.php
+++ project/src/Controller/Blog/PostController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller\Blog;
 
+use App\Entity\Post\Post;
 use App\Repository\Post\PostRepository;
-use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends AbstractController
 {
@@ -18,6 +18,14 @@ class PostController extends AbstractController
     ): Response {
         return $this->render('pages/blog/index.html.twig', [
             'posts' => $postRepository->findPublished($request->query->getInt('page', 1))
+        ]);
+    }
+
+    #[Route('/article/{slug}', name: 'post.show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('pages/blog/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
